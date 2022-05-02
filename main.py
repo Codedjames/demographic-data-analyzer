3557ca46376886979b05fff5d0c350cac9354a17
Oladejo James <34400118+Codedjames@users.noreply.github.com>
2022-05-02 04:47:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import demographic_data_analyzer
+from unittest import main
+
+# This entrypoint file to be used in development. Start by reading README.md
+
+# Test your function by calling it here
+demographic_data_analyzer.calculate_demographic_data()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
